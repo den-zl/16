87ea8337e5f860a20622d5e6cc7936599598e4a3
den-zl <lashindeniskb232@gmail.com>
2024-05-06 00:12:03 +0300

    sortColsByMinElement

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -76,5 +76,8 @@ void swapRowsByMinMaxElements(matrix m);
 int getMax(int *a, int n);
 void sortRowsByMinElement(matrix m);
 
+int getMin(int *a, int n);
+void sortColsByMinElement(matrix m);
+
 
 #endif
